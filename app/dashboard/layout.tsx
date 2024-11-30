2b759eb24ddc66ca77fc78ff130151f20a270884
halobartku <bartosz@easykotlet.com>
2024-11-30 11:37:05 +0100

    fix: Update import path for dashboard-top-nav component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode, Suspense } from "react"
 import DashboardSideBar from "./_components/dashboard-side-bar"
-import DashboardTopNav from "./_components/dashbord-top-nav"
+import DashboardTopNav from "./_components/dashboard-top-nav"
 import { isAuthorized } from "@/utils/data/user/isAuthorized"
 import { redirect } from "next/navigation"
 import { currentUser } from "@clerk/nextjs/server"
